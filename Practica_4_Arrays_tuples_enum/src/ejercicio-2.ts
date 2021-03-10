5f27e50b5712603322d94c7f62b273baca917ae8
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-10 19:40:14 +0000

    Falló test ejercicio-2

---
+++ Practica_4_Arrays_tuples_enum/src/ejercicio-2.ts
@@ -0,0 +1,7 @@
+function meshArray(arrayStr: String[]){
+  let resultado = '';
+}
+
+const strArray: String[] = ['allow', 'lowering', 'ringmaster', 'terror'];
+console.log(`El array es: ${strArray}`);
+console.log(`Resultado: ${meshArray(strArray)}`);
---
+++ Practica_4_Arrays_tuples_enum/tests/ejercicio-2.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import {expect} from 'chai';
+import {meshArray} from '../src/ejercicio-2';
+
+describe('decodificar sentencias', () => {
+  it('decodeResistor(marrón, verde) returns value 15', () => {
+    expect(meshArray("marrón","verde")).to.be.equal(15);
+  });
+});
